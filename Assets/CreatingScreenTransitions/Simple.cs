f3ec4250f0d171d01016edcecc1d965851a20c08
james <james@spe3d.co>
2016-01-27 18:32:24 +0800

    Stash

---
+++ Assets/CreatingScreenTransitions/Simple.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Simple : MonoBehaviour
+{
+	public void LoadScene2 ()
+	{
+		SceneManager.LoadScene ("Screen2");
+	}
+}
