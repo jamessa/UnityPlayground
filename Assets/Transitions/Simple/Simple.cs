93c477f5b5c44b3a86ba99123e1181b1b792ecbc
james <james@spe3d.co>
2016-01-28 16:19:35 +0800

    Simple Transition

---
+++ Assets/Transitions/Simple/Simple.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Simple : MonoBehaviour
+{
+	public void LoadScene (string sceneString)
+	{
+		SceneManager.LoadScene (sceneString);
+	}
+}
