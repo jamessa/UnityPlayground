13df8e7d74649f21684d01ab17f40deeff676a86
james <james@spe3d.co>
2016-02-05 17:34:26 +0800

    Debug mode testing

---
+++ Assets/Debug Me/Editor/SomeBuildScript.cs
@@ -0,0 +1,18 @@
+﻿// Editor Script must live in /Editor Folder to work nicely with Debug mode.
+
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+
+public class SomeBuildScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("OnPostprocessBuildiOS");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
