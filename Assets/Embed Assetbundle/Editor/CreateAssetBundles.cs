3a5704fde4adb94b7084cb3713f62cbb6f6d02ee
james <james@spe3d.co>
2016-02-07 01:12:32 +0800

    Embed asset bundle in streamingassets. This makes create a bundle then embed it in the build possible.

---
+++ Assets/Embed Assetbundle/Editor/CreateAssetBundles.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateAssetBundles
+{
+	[MenuItem ("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles ()
+	{
+		Debug.Log( "Saving to " + Application.streamingAssetsPath );
+		BuildPipeline.BuildAssetBundles ( Application.streamingAssetsPath );
+	}
+}
---
+++ Assets/Embed Assetbundle/LoadStreamingAssets.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadStreamingAssets : MonoBehaviour {
+	string filePath;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		filePath = "file://" + System.IO.Path.Combine ( Application.streamingAssetsPath, "basic");
+		print (filePath);
+
+		using (WWW www = WWW.LoadFromCacheOrDownload (filePath, -1)) {
+			yield return www;
+
+			AssetBundle bundle = www.assetBundle;
+
+			Instantiate (bundle.LoadAsset("Cube"));
+		}
+
+	}
+}
+
+				
