e8e736c301fef8004f71f0d072e31733763fe401
james <james.sa.2013@gmail.com>
2016-02-16 14:41:40 +0800

    Coroutine Examples

---
+++ Assets/Coroutines/CoroutinesExample.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutinesExample : MonoBehaviour
+{
+	public float smoothing = 1f;
+	public Transform target;
+
+	void Start ()
+	{
+		StartCoroutine (MyCoroutine (target));
+	}
+
+	IEnumerator MyCoroutine (Transform t)
+	{
+		while (Vector3.Distance (transform.position, t.position) > 0.05f) {
+			transform.position = Vector3.Lerp (transform.position, t.position, smoothing * Time.deltaTime);
+			Debug.Log ("Moving toward target. " + Vector3.Distance (transform.position, t.position));
+			yield return null;
+		}
+	
+		Debug.Log ("Reached the target.");
+
+		yield return new WaitForSeconds (3f);
+
+		Debug.Log ("MyCoroutine is now finished");
+	}
+
+}
+
---
+++ Assets/Coroutines/README.md
@@ -0,0 +1,10 @@
+#Coroutine & yield
+
+Corountine is Unity's solution to asynchrounous operations. However, there's an mystery `yield return null` vs `yield return WaitForEndOfFrame`.
+
+They'r triggered at different time. http://answers.unity3d.com/questions/755196/yield-return-null-vs-yield-return-waitforendoffram.html
+
+#Coroutine vs Monobehavior
+
+http://answers.unity3d.com/questions/161084/coroutine-without-monobehaviour.html
+http://answers.unity3d.com/questions/932786/coroutine-1.html
