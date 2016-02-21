9880abbc2618e321835bbe9dfa8b8bfe79957b3b
james <james.sa.2013@gmail.com>
2016-02-21 16:14:15 +0800

    Simple Async Loading

---
+++ Assets/Loading/LoadingController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadingController : MonoBehaviour
+{
+
+	public GameObject LoadingScene;
+
+
+	protected void OnGUI ()
+	{
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, 5, 200, 30), "Load Asynchronously")) {
+			StartCoroutine (LoadingAsynchronously ());
+		}
+	}
+
+	IEnumerator LoadingAsynchronously ()
+	{
+		LoadingScene.SetActive (true);
+		AsyncOperation async = SceneManager.LoadSceneAsync ("Loading Second Scene");
+
+		while (!async.isDone) {
+			print ("loading progress: " + async.progress);
+			yield return null;
+		}
+	}
+}
