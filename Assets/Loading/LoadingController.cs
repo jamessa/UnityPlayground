2646c482c225d523e2a748d54ab2e36d0cef6baa
james <james.sa.2013@gmail.com>
2016-02-21 16:46:35 +0800

    Loading Scene by LoadAdditive mode

--- Assets/Loading/LoadingController.cs
+++ Assets/Loading/LoadingController.cs
@@ -1,29 +1,52 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class LoadingController : MonoBehaviour
 {
-
-	public GameObject LoadingScene;
-
+	public GameObject LoadingScenePanel;
 
 	protected void OnGUI ()
 	{
-		if (GUI.Button (new Rect (Screen.width / 2 - 100, 5, 200, 30), "Load Asynchronously")) {
-			StartCoroutine (LoadingAsynchronously ());
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 3, 200, 30), "Load Asynchronously")) {
+			StartCoroutine (LoadAsynchronously ());
+		}
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height * 2 / 3, 200, 30), "Load with Additive Scene")) {
+			StartCoroutine (LoadWithAdditiveScene ());
 		}
 	}
 
-	IEnumerator LoadingAsynchronously ()
+	IEnumerator LoadAsynchronously ()
 	{
-		LoadingScene.SetActive (true);
+		LoadingScenePanel.SetActive (true);
+		yield return new WaitForSeconds (2); // just for demo
 		AsyncOperation async = SceneManager.LoadSceneAsync ("Loading Second Scene");
 
 		while (!async.isDone) {
 			print ("loading progress: " + async.progress);
 			yield return null;
+		}
+	}
+
+	IEnumerator LoadWithAdditiveScene ()
+	{
+		print ("Loading additive loading scene");
+		SceneManager.LoadScene ("Loading Scene", LoadSceneMode.Additive);
+		yield return new WaitForSeconds (2); // just for demo
+
+		AsyncOperation async = SceneManager.LoadSceneAsync ("Loading Second Scene");
+		async.allowSceneActivation = false;
+		while (!async.isDone) {
+			print ("loading progress: " + async.progress);
+			yield return new WaitForSeconds (1); // just for demo
+
+			// Sometime you're not only wait for a scene but also other
+			// long operation like download, you can put them here.
+			if (async.progress >= 0.9f) {
+				print ("Second scene loaded.");
+				async.allowSceneActivation = true;
+			}
+
 		}
 	}
 }
