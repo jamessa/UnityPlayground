f5e1b778215c6a59f81ff0604053ed9f4eb8a1b3
james <james.sa.2013@gmail.com>
2016-02-24 22:27:36 +0800

    Update for State Machine.

---
+++ Assets/StateMachine/README.md
@@ -0,0 +1,5 @@
+#State Machine
+
+State Machine is a great way to make your program simpler. Here's a really simple example that uses Prime 31's [StateKit](https://github.com/prime31/StateKit) to achieve state machine.
+ 
+![StateMachine](StateMachine.gif)
--- Assets/StateMachine/StateMachineController.cs
+++ Assets/StateMachine/StateMachineController.cs
@@ -5,11 +5,11 @@ namespace Example.State
 {
 	public enum Player
 	{
-		Walking,
-		Idle
+		Work,
+		Play
 	}
 
-	public class StateSceneController : StateKitLite<Player>
+	public class StateMachineController : StateKitLite<Player>
 	{
 
 		// if you implement Awake in your subclass you must call base.Awake()
@@ -21,7 +21,7 @@ namespace Example.State
 		// in either Awake or Start the initialState must be set
 		void Start ()
 		{
-			initialState = Player.Idle;
+			initialState = Player.Play;
 		}
 
 		// if you implement Update in your subclass you must call base.Update()
@@ -32,46 +32,46 @@ namespace Example.State
 
 		protected void OnGUI ()
 		{
-			if (GUI.Button (new Rect (Screen.width * 1 / 3 - 50, 5, 100, 30), "Walk")) {
-				currentState = Player.Walking;
+			if (GUI.Button (new Rect (Screen.width * 1 / 3 - 50, 5, 100, 30), "Work")) {
+				currentState = Player.Work;
 			}
 
-			if (GUI.Button (new Rect (Screen.width * 2 / 3 - 50, 5, 100, 30), "Idle")) {
-				currentState = Player.Idle;
+			if (GUI.Button (new Rect (Screen.width * 2 / 3 - 50, 5, 100, 30), "Play")) {
+				currentState = Player.Play;
 			}
 			var centeredStyle = GUI.skin.GetStyle ("Label");
 			centeredStyle.alignment = TextAnchor.MiddleCenter;
 			GUI.Label (new Rect (Screen.width / 2 - 50, Screen.height / 4, 100, 30), currentState.ToString (), centeredStyle);
 		}
 
-		void Walking_Enter ()
+		void Work_Enter ()
 		{
-			print ("Walk enter.");
+			print ("Work enter.");
 		}
 
-		void Walking_Tick ()
+		void Work_Tick ()
 		{
-//		print ("Walking");
+//		print ("Working");
 		}
 
-		void Walking_Exit ()
+		void Work_Exit ()
 		{
-			print ("Walk exit.");
+			print ("Work exit.");
 		}
 
-		void Idle_Enter ()
+		void Play_Enter ()
 		{
-			print ("Idle enter.");
+			print ("Play enter.");
 		}
 
-		void Idle_Tick ()
+		void Play_Tick ()
 		{
 //		print ("Idling");
 		}
 
-		void Idle_Exit ()
+		void Play_Exit ()
 		{
-			print ("Idle exit.");
+			print ("Play exit.");
 		}
 	}
 }
