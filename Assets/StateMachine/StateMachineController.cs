1019a5a130ea3e291664bfc99258ef63efe273bc
james <james.sa.2013@gmail.com>
2016-02-24 22:36:30 +0800

    Update format

--- Assets/StateMachine/StateMachineController.cs
+++ Assets/StateMachine/StateMachineController.cs
@@ -13,65 +13,65 @@ namespace Example.State
 	{
 
 		// if you implement Awake in your subclass you must call base.Awake()
-		void Awake ()
+		void Awake()
 		{
-			base.Awake ();
+			base.Awake();
 		}
 
 		// in either Awake or Start the initialState must be set
-		void Start ()
+		void Start()
 		{
 			initialState = Player.Play;
 		}
 
 		// if you implement Update in your subclass you must call base.Update()
-		void Update ()
+		void Update()
 		{
-			base.Update ();
+			base.Update();
 		}
 
-		protected void OnGUI ()
+		protected void OnGUI()
 		{
-			if (GUI.Button (new Rect (Screen.width * 1 / 3 - 50, 5, 100, 30), "Work")) {
+			if (GUI.Button(new Rect(Screen.width * 1 / 3 - 50, 5, 100, 30), "Work")) {
 				currentState = Player.Work;
 			}
 
-			if (GUI.Button (new Rect (Screen.width * 2 / 3 - 50, 5, 100, 30), "Play")) {
+			if (GUI.Button(new Rect(Screen.width * 2 / 3 - 50, 5, 100, 30), "Play")) {
 				currentState = Player.Play;
 			}
-			var centeredStyle = GUI.skin.GetStyle ("Label");
+			var centeredStyle = GUI.skin.GetStyle("Label");
 			centeredStyle.alignment = TextAnchor.MiddleCenter;
-			GUI.Label (new Rect (Screen.width / 2 - 50, Screen.height / 4, 100, 30), currentState.ToString (), centeredStyle);
+			GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 4, 100, 30), currentState.ToString(), centeredStyle);
 		}
 
-		void Work_Enter ()
+		void Work_Enter()
 		{
-			print ("Work enter.");
+			print("Work enter.");
 		}
 
-		void Work_Tick ()
+		void Work_Tick()
 		{
 //		print ("Working");
 		}
 
-		void Work_Exit ()
+		void Work_Exit()
 		{
-			print ("Work exit.");
+			print("Work exit.");
 		}
 
-		void Play_Enter ()
+		void Play_Enter()
 		{
-			print ("Play enter.");
+			print("Play enter.");
 		}
 
-		void Play_Tick ()
+		void Play_Tick()
 		{
 //		print ("Idling");
 		}
 
-		void Play_Exit ()
+		void Play_Exit()
 		{
-			print ("Play exit.");
+			print("Play exit.");
 		}
 	}
 }
