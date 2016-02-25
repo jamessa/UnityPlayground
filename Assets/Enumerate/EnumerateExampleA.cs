a2e22a9750e2ea0c5909be7e7c6063dc86e21355
james <james.sa.2013@gmail.com>
2016-02-25 11:02:07 +0800

    First Enumerator

---
+++ Assets/Enumerate/EnumerateExampleA.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnumerateExampleA : MonoBehaviour
+{
+	public int Power(int number, int exponent)
+	{
+		int counter = 0;
+		int result = 1;
+		while (counter++ < exponent) {
+			result *= number;
+		}
+		return result;
+	}
+
+	public static IEnumerable EPower(int number, int exponent)
+	{
+		int counter = 0;
+		int result = 1;
+		while (counter++ < exponent) {
+			result *= number;
+			yield return result;
+		}
+	}
+
+	void Start()
+	{
+		for (int i = 1; i <= 8; i++) {
+			print(Power(2, i));
+		}
+			
+		foreach (int i in EPower(2,8)) {
+			print(i);
+		}
+	}
+}
---
+++ Assets/Unity.Blog.Understanding_IEnumerator/IEnumerator_Example_C.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace EPPZ.Blog.IEnumerator_Example_C
+{
+
+
+	public class Polygon
+	{
+
+
+		public Vector2[] points;
+		public Polygon[] polygons;
+
+
+		public IEnumerator PointEnumerator()
+		{
+			// Enumerate local points.
+			foreach (Vector2 eachPoint in points)
+			{
+				yield return eachPoint;
+			}
+			
+			// Enumerate each sub-polygon points.
+			foreach (Polygon eachPolygon in polygons)
+			{
+				IEnumerator pointEnumerator = eachPolygon.PointEnumerator();
+				while (pointEnumerator.MoveNext())
+				{
+					yield return pointEnumerator.Current;
+				}
+			}
+			
+			yield break;
+		}
+	}
+
+
+	public class IEnumerator_Example_C : MonoBehaviour
+	{
+
+
+		public Polygon polygon; // Setup in inspector
+
+
+		void Start()
+		{
+			float start = Time.realtimeSinceStartup;
+			for (int i = 0; i < 100000; i++)
+			{
+				// Enumerate points (recursive).
+				IEnumerator pointEnumerator = polygon.PointEnumerator ();
+				while (pointEnumerator.MoveNext())
+				{
+					// Debug.Log (pointEnumerator.Current);
+				}
+			}
+			float end = Time.realtimeSinceStartup;
+			Debug.Log("100000 enumerations using IEnumerator: "+(end - start)+" ms lapsed ().");
+		}
+	}
+}
